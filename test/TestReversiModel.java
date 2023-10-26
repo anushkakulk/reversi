13304fe211b846e713b39ce744b6ead1b741c703
Hbadmus <128654061+Hbadmus@users.noreply.github.com>
2023-10-26 00:15:39 -0400

    added testMove

--- test/TestReversiModel.java
+++ test/TestReversiModel.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertTrue;
+
 public class TestReversiModel {
 
   private ReversiModel model;
@@ -53,6 +55,42 @@ public class TestReversiModel {
             new Tile(2, 0, -2)
     );
     Assert.assertEquals(expectedBoard, model.getBoard(3));
+  }
+
+  @Test
+  public void testMove() {
+    List<Tile> board = Arrays.asList(
+        new Tile(-2, 0, 2),
+        new Tile(-2, 1, 1),
+        new Tile(-2, 2, 0),
+        new Tile(-1, -1, 2),
+        new Tile(-1, 0, 1),
+        new Tile(-1, 1, 0),
+        new Tile(-1, 2, -1),
+        new Tile(0, -2, 2),
+        new Tile(0, -1, 1),
+        new Tile(0, 0, 0),
+        new Tile(0, 1, -1),
+        new Tile(0, 2, -2),
+        new Tile(1, -2, 1),
+        new Tile(1, -1, 0),
+        new Tile(1, 0, -1),
+        new Tile(1, 1, -2),
+        new Tile(2, -2, 0),
+        new Tile(2, -1, -1),
+        new Tile(2, 0, -2)
+    );
+
+    model.startGame(board);
+
+    model.move(1, -2, 1);
+
+    assertTrue(model.getPieceAt(1,-1, 0) == ReversiPiece.BLACK);
+    assertTrue(model.getPieceAt(1,-2, 1) == ReversiPiece.BLACK);
+
+
+
+
   }
 
 
