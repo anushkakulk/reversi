3ae12a6b65ecc9fcc0d9b27aadd493d7ff87acd8
Hbadmus <Hbadmus1@gmail.com>
2023-11-27 23:06:26 -0500

    finished mocks

--- src/cs3500/reversi/controller/MockLogReversiGameController.java
+++ src/cs3500/reversi/controller/MockLogReversiGameController.java
@@ -2,6 +2,10 @@ package cs3500.reversi.controller;
 
 import cs3500.reversi.model.ReversiModel;
 import cs3500.reversi.model.ReversiPiece;
+import cs3500.reversi.player.Player;
+import cs3500.reversi.view.ReversiView;
+
+import java.util.Objects;
 
 /**
  * Represents a Mock for a ReversiController that will log input to any method calls
@@ -12,14 +16,32 @@ public class MockLogReversiGameController implements IReversiController, PlayerA
   private ReversiController actualController;
   private final StringBuilder log;
 
+  private final ReversiPiece myPlayer;
+
+  private final ReversiModel model;
+  private final ReversiView view;
+
+  private final Player player;
   /**
    * Creates an instance of this mock type.
-   * @param c an actual ReversiController that can be played
-   * @param log the log that will contain a transcript of any inputs to any methods
+   *
+   * @param log       the log that will contain a transcript of any inputs to any methods
+   * @param c         an actual ReversiController that can be played
+   * @param myPlayer  the player that this mock is playing as
    */
-  public MockLogReversiGameController(StringBuilder log, ReversiController c) {
+  public MockLogReversiGameController(StringBuilder log, Player player, ReversiController c, ReversiPiece myPlayer,
+      ReversiModel model, ReversiView view) {
+
     this.actualController = c;
     this.log = log;
+    this.myPlayer = myPlayer;
+    this.player = Objects.requireNonNull(player);
+    this.model = Objects.requireNonNull(model);
+    this.view = Objects.requireNonNull(view);
+    this.model.addModelStatusListener(this);
+    this.view.addPlayerActionListener(this);
+    this.player.addPlayerActionListener(this);
+
   }
 
   @Override
@@ -48,13 +70,16 @@ public class MockLogReversiGameController implements IReversiController, PlayerA
 
   @Override
   public void handleMoveChosen(int xCoord, int rCoord, int sCoord) {
-    log.append("handleMoveChosen: ").append(xCoord).append(", ").append(rCoord).append(", ").
-        append(sCoord).append("\n");
+    log.append("MyPlayer: ").append(myPlayer).append(", ").append("Move Chosen: ")
+        .append(xCoord).append(", ").append(rCoord).append(", ").append(sCoord)
+        .append("\n");
         actualController.handleMoveChosen(xCoord, rCoord, sCoord);
   }
 
   @Override
   public void handlePassChosen() {
-
+    log.append("MyPlayer: ").append(myPlayer).append(", ").append("Pass Chosen")
+        .append("\n");
+    actualController.handlePassChosen();
   }
 }
--- test/cs3500/reversi/controller/TestReversiController.java
+++ test/cs3500/reversi/controller/TestReversiController.java
@@ -1,8 +1,6 @@
 package cs3500.reversi.controller;
 
-import cs3500.reversi.player.CaptureMostStrategy;
-import cs3500.reversi.player.Player;
-import cs3500.reversi.player.Strategy;
+import cs3500.reversi.player.*;
 import cs3500.reversi.view.ReversiGUIView;
 import cs3500.reversi.view.ReversiTextualView;
 import cs3500.reversi.view.ReversiView;
@@ -27,83 +25,200 @@ import cs3500.reversi.model.ReversiPiece;
 public class TestReversiController {
 
   private ReversiController c;
+  private ReversiController c2;
   private ReversiModel m;
   private ReversiView v;
+  private MockLogReversiGameController mock;
+  private MockLogReversiGameController mock2;
 
-  @Before
-  public void setUp() {
+  @Test
+  public void testControllerAvoidNextToCornerVSCaptureMost() {
     m = new ReversiGameModel(6);
-    m.startGame();
     v = new ReversiGUIView(m);
-    c = new ReversiController(m,
-        new Player(new Strategy(new CaptureMostStrategy()), ReversiPiece.BLACK),
-        v);
+
+    StringBuilder out = new StringBuilder();
+
+    Strategy avoidStrat = new Strategy(new AvoidNextToCornersStrategy());
+    Player autoPlayer = new Player(avoidStrat, ReversiPiece.BLACK);
+
+    Strategy capStrat = new Strategy(new CaptureMostStrategy());
+    Player autoPlayer2 = new Player(capStrat, ReversiPiece.WHITE);
+
+    c = new ReversiController(m, autoPlayer, v);
+    c2 = new ReversiController(m, autoPlayer2, v);
+
+    mock = new MockLogReversiGameController(out, autoPlayer, c, ReversiPiece.BLACK, m, v);
+    mock2 = new MockLogReversiGameController(out, autoPlayer2, c2, ReversiPiece.WHITE, m, v);
+
+    m.startGame();
+
+    Assert.assertTrue(out.toString().contains("MyPlayer: X, Move Chosen: -1, -1, 2"));
+    Assert.assertTrue(out.toString().contains("MyPlayer: O, Move Chosen: -4, 3, 1"));
+    Assert.assertTrue(out.toString().contains("MyPlayer: X, Move Chosen: 2, 1, -3"));
+    Assert.assertTrue(out.toString().contains("MyPlayer: O, Move Chosen: 1, -4, 3"));
+    Assert.assertTrue(out.toString().contains("MyPlayer: X, Pass Chosen\n" +
+        "MyPlayer: O, Pass Chosen"));
+
   }
 
   @Test
-  public void testHandlePlayerChange() {
+  public void testControllerCaptureMostBoth() {
+    m = new ReversiGameModel(6);
+    v = new ReversiGUIView(m);
+
     StringBuilder out = new StringBuilder();
-    MockLogReversiGameController mock = new MockLogReversiGameController(out, c);
 
-    mock.handlePlayerChange(ReversiPiece.BLACK);
+    Strategy avoidStrat = new Strategy(new CaptureMostStrategy());
+    Player autoPlayer = new Player(avoidStrat, ReversiPiece.BLACK);
 
-    Assert.assertTrue(out.toString().contains("handlePlayerChange: X"));
-    Assert.assertFalse(out.toString().contains("handlePlayerChange: O"));
+    Strategy capStrat = new Strategy(new CaptureMostStrategy());
+    Player autoPlayer2 = new Player(capStrat, ReversiPiece.WHITE);
 
-    mock.handlePlayerChange(ReversiPiece.WHITE);
+    c = new ReversiController(m, autoPlayer, v);
+    c2 = new ReversiController(m, autoPlayer2, v);
 
-    Assert.assertTrue(out.toString().contains("handlePlayerChange: X"));
-    Assert.assertTrue(out.toString().contains("handlePlayerChange: O"));
+    mock = new MockLogReversiGameController(out, autoPlayer, c, ReversiPiece.BLACK, m, v);
+    mock2 = new MockLogReversiGameController(out, autoPlayer2, c2, ReversiPiece.WHITE, m, v);
+
+    m.startGame();
+
+    Assert.assertTrue(out.toString().contains("MyPlayer: X, Move Chosen: -1, -1, 2"));
+    Assert.assertTrue(out.toString().contains("MyPlayer: O, Move Chosen: -2, 1, 1"));
+    Assert.assertTrue(out.toString().contains("MyPlayer: X, Move Chosen: -3, -1, 4"));
+    Assert.assertTrue(out.toString().contains("MyPlayer: O, Move Chosen: -3, 0, 3"));
+    Assert.assertTrue(out.toString().contains("MyPlayer: O, Pass Chosen\n" +
+        "MyPlayer: X, Pass Chosen"));
 
   }
 
-    @Test
-  public void testHandleTileClicked() {
+  @Test
+  public void testControllerAvoidCornersBoth() {
+    m = new ReversiGameModel(6);
+    v = new ReversiGUIView(m);
+
     StringBuilder out = new StringBuilder();
-    MockLogReversiGameController mock = new MockLogReversiGameController(out, c);
 
-    mock.handleTileClicked(0, 0, 0);
+    Strategy avoidStrat = new Strategy(new AvoidNextToCornersStrategy());
+    Player autoPlayer = new Player(avoidStrat, ReversiPiece.BLACK);
 
-    Assert.assertTrue(out.toString().contains("handleTileClicked: 0, 0, 0"));
+    Strategy capStrat = new Strategy(new AvoidNextToCornersStrategy());
+    Player autoPlayer2 = new Player(capStrat, ReversiPiece.WHITE);
 
-      mock.handleTileClicked(-1, -1, 2);
-      mock.handleTileClicked(2, -1, -1);
+    c = new ReversiController(m, autoPlayer, v);
+    c2 = new ReversiController(m, autoPlayer2, v);
 
-      Assert.assertTrue(out.toString().contains("handleTileClicked: 0, 0, 0"));
-      Assert.assertTrue(out.toString().contains("handleTileClicked: -1, -1, 2"));
-      Assert.assertTrue(out.toString().contains("handleTileClicked: 2, -1, -1"));
+    mock = new MockLogReversiGameController(out, autoPlayer, c, ReversiPiece.BLACK, m, v);
+    mock2 = new MockLogReversiGameController(out, autoPlayer2, c2, ReversiPiece.WHITE, m, v);
+
+    m.startGame();
+
+    Assert.assertTrue(out.toString().contains("MyPlayer: X, Move Chosen: -1, -1, 2"));
+    Assert.assertTrue(out.toString().contains("MyPlayer: O, Move Chosen: -2, 1, 1"));
+    Assert.assertFalse(out.toString().contains("MyPlayer: X, Move Chosen: -5, 0, 5"));
+    Assert.assertFalse(out.toString().contains("MyPlayer: O, Move Chosen: 0, 5, -5"));
+    Assert.assertFalse(out.toString().contains("MyPlayer: O, Move Chosen: 5, -1, -4"));
+    Assert.assertFalse(out.toString().contains("MyPlayer: O, Move Chosen: 0, 4, -4"));
+    Assert.assertFalse(out.toString().contains("MyPlayer: O, Move Chosen: -4, 0, 4"));
+
 
   }
 
-@Test
-    public void testHandleMoveChosen() {
-        StringBuilder out = new StringBuilder();
-        MockLogReversiGameController mock = new MockLogReversiGameController(out, c);
+  @Test
+  public void testControllerAvoidCornersVSPlayCornersAndCaptureMost() {
+    m = new ReversiGameModel(6);
+    v = new ReversiGUIView(m);
 
-        mock.handleMoveChosen(0, 0, 0);
+    StringBuilder out = new StringBuilder();
 
-        Assert.assertTrue(out.toString().contains("handleMoveChosen: 0, 0, 0"));
+    Strategy avoidStrat = new Strategy(new AvoidNextToCornersStrategy());
+    Player autoPlayer = new Player(avoidStrat, ReversiPiece.BLACK);
 
-        mock.handleMoveChosen(-1, -1, 2);
-        mock.handleMoveChosen(2, -1, -1);
+    List<IPlayerMoveStrategy> strategyList = Arrays.asList(
+        new PlayCornersStrategy(), new CaptureMostStrategy());
+    Strategy combo = new Strategy(new ManyStrategy(strategyList));
+    Player autoPlayer2 = new Player(combo, ReversiPiece.WHITE);
 
-        Assert.assertTrue(out.toString().contains("handleMoveChosen: 0, 0, 0"));
-        Assert.assertTrue(out.toString().contains("handleMoveChosen: -1, -1, 2"));
-        Assert.assertTrue(out.toString().contains("handleMoveChosen: 2, -1, -1"));
+    c = new ReversiController(m, autoPlayer, v);
+    c2 = new ReversiController(m, autoPlayer2, v);
 
-    }
+    mock = new MockLogReversiGameController(out, autoPlayer, c, ReversiPiece.BLACK, m, v);
+    mock2 = new MockLogReversiGameController(out, autoPlayer2, c2, ReversiPiece.WHITE, m, v);
 
-    @Test
-    public void testHandlePlayerAction() {
-        StringBuilder out = new StringBuilder();
-        MockLogReversiGameController mock = new MockLogReversiGameController(out, c);
+    m.startGame();
 
-        mock.handlePlayerChange(ReversiPiece.BLACK);
-        mock.handlePlayerAction(() -> m.move(-1, -1, 2));
+    Assert.assertTrue(out.toString().contains("MyPlayer: X, Move Chosen: 0, 3, -3"));
+    Assert.assertTrue(out.toString().contains("MyPlayer: O, Move Chosen: 5, -4, -1"));
+    Assert.assertTrue(out.toString().contains("MyPlayer: X, Move Chosen: -1, 2, -1"));
+    Assert.assertTrue(out.toString().contains("MyPlayer: O, Move Chosen: 3, -3, 0"));
+    Assert.assertTrue(out.toString().contains("MyPlayer: O, Pass Chosen\n" +
+        "MyPlayer: X, Pass Chosen"));
 
-        Assert.assertTrue(out.toString().contains("handlePlayerAction: () -> m.move(-1, -1, 2)"));
+  }
 
-    }
+  @Test
+  public void testControllerCaptureMostVSPlayCornersAndAvoidCorners() {
+    m = new ReversiGameModel(6);
+    v = new ReversiGUIView(m);
+
+    StringBuilder out = new StringBuilder();
+
+    Strategy avoidStrat = new Strategy(new CaptureMostStrategy());
+    Player autoPlayer = new Player(avoidStrat, ReversiPiece.BLACK);
+
+    List<IPlayerMoveStrategy> strategyList = Arrays.asList(
+        new PlayCornersStrategy(), new AvoidNextToCornersStrategy());
+    Strategy combo = new Strategy(new ManyStrategy(strategyList));
+    Player autoPlayer2 = new Player(combo, ReversiPiece.WHITE);
+
+    c = new ReversiController(m, autoPlayer, v);
+    c2 = new ReversiController(m, autoPlayer2, v);
+
+    mock = new MockLogReversiGameController(out, autoPlayer, c, ReversiPiece.BLACK, m, v);
+    mock2 = new MockLogReversiGameController(out, autoPlayer2, c2, ReversiPiece.WHITE, m, v);
+
+    m.startGame();
+
+    Assert.assertTrue(out.toString().contains("MyPlayer: X, Move Chosen: -1, -1, 2"));
+    Assert.assertTrue(out.toString().contains("MyPlayer: O, Move Chosen: -2, 1, 1"));
+    Assert.assertTrue(out.toString().contains("MyPlayer: X, Move Chosen: 5, -4, -1"));
+    Assert.assertTrue(out.toString().contains("MyPlayer: O, Move Chosen: -5, 2, 3"));
+
+  }
+
+  @Test
+  public void testControllerAvoidCornersAndCaptureMostVSPlayCornersAndCaptureMost() {
+    m = new ReversiGameModel(6);
+    v = new ReversiGUIView(m);
+
+    StringBuilder out = new StringBuilder();
+
+    List<IPlayerMoveStrategy> strategyList1 = Arrays.asList(
+        new AvoidNextToCornersStrategy(), new CaptureMostStrategy());
+    Strategy combo1 = new Strategy(new ManyStrategy(strategyList1));
+    Player autoPlayer = new Player(combo1, ReversiPiece.BLACK);
+
+    List<IPlayerMoveStrategy> strategyList2 = Arrays.asList(
+        new PlayCornersStrategy(), new CaptureMostStrategy());
+    Strategy combo2 = new Strategy(new ManyStrategy(strategyList2));
+    Player autoPlayer2 = new Player(combo2, ReversiPiece.WHITE);
+
+    c = new ReversiController(m, autoPlayer, v);
+    c2 = new ReversiController(m, autoPlayer2, v);
+
+    mock = new MockLogReversiGameController(out, autoPlayer, c, ReversiPiece.BLACK, m, v);
+    mock2 = new MockLogReversiGameController(out, autoPlayer2, c2, ReversiPiece.WHITE, m, v);
+
+    m.startGame();
+
+    Assert.assertTrue(out.toString().contains("MyPlayer: X, Move Chosen: -1, -1, 2"));
+    Assert.assertTrue(out.toString().contains("MyPlayer: O, Move Chosen: -2, 1, 1"));
+    Assert.assertTrue(out.toString().contains("MyPlayer: X, Move Chosen: 0, 3, -3"));
+    Assert.assertTrue(out.toString().contains("MyPlayer: O, Move Chosen: 3, -3, 0"));
+    Assert.assertTrue(out.toString().contains("MyPlayer: X, Pass Chosen\n" +
+        "MyPlayer: O, Pass Chosen"));
+
+  }
+
 
 
 }
